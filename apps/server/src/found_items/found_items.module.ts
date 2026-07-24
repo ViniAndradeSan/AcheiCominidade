6f34c2459cba4e9851897b6641c04dbda7a70590
Vinicius <viniciusandrade0521@gmail.com>
2026-07-23 21:42:04 -0300

    fix: add explicit PrismaModule imports to all feature modules

--- apps/server/src/found_items/found_items.module.ts
+++ apps/server/src/found_items/found_items.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { FoundItemsController } from "./found_items.controller";
 import { FoundItemsService } from "./found_items.service";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
+	imports: [PrismaModule],
 	controllers: [FoundItemsController],
 	providers: [FoundItemsService],
 })
--- apps/server/src/item_returns/item_returns.module.ts
+++ apps/server/src/item_returns/item_returns.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ItemReturnsController } from "./item_returns.controller";
 import { ItemReturnsService } from "./item_returns.service";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
+	imports: [PrismaModule],
 	controllers: [ItemReturnsController],
 	providers: [ItemReturnsService],
 })
